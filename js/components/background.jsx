69ef81f6d8ccfe53ad5b0e2af525bcc0e0ab3dca
Michal Karbasz <michal.karbasz@gmail.com>
2018-06-29 18:59:02 +0200

    minor background changes

--- js/components/background.jsx
+++ js/components/background.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Background extends React.Component {
     render() {
         return (
-            <div>
+            <div className='main-container'>
                 <div className="background"></div>
             </div>
         )
